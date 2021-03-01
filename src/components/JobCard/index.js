4ad1b6e91e8a338bc1109750227838782bfd2313
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-03-02 00:14:19 +0300

    jobCard component created.

---
+++ src/components/JobCard/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import styles from './JobCard.module.css';
+
+const JobCard = ({ job, setCurrentJobId }) => {
+
+
+	const {id, Job_title, City, Company, Rating} = job;
+
+
+	
+
+	return (
+	
+	<div className={styles.card}  >
+		<h2 className={styles.title} value={job.id} id={id}  onClick={(e) => setCurrentJobId(e.target.id)} >{Job_title}</h2>
+		<p className={styles.category_name}>{Company}</p>
+		<p className={styles.state_info}>{City}</p>
+		<span>Rating: {Rating }</span>
+	</div>
+	)
+}
+
+export default JobCard;
