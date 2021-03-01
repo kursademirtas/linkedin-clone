ea430351f170d2b5cc4c879258783210356543d3
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-03-02 00:14:38 +0300

    new components added.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-//import Header from "./components/Header";
 import Feed from './pages/feed'
 import Login from "./pages/login";
 import Homepage from  './pages/homepage'
 import { AuthProvider } from "./contexts/AuthContext";
+import { JobDataProvider } from './contexts/JobsDataContext';
 import PrivateRoute from "./components/PrivateRoute";
+import Signup from "./pages/signup/Signup";
+import Jobs from './pages/jobs'
+
 
 function App() {
 
-
   return (
     
-      <>
+      <div className="app">
       <Router>
-        <AuthProvider >
-          <Switch>
-             <Route path="/" exact component={Homepage} />
-             <Route path="/login"  component={Login} />
-             <PrivateRoute path="/feed" component={Feed} />
-          </Switch>
-          </AuthProvider>
+        <JobDataProvider>
+          <AuthProvider >
+            <Switch>
+              <Route path="/" exact component={Homepage} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login"  component={Login} />
+              <Route path="/jobs" component={Jobs} />
+              <PrivateRoute path="/feed" component={Feed} />
+            </Switch>
+            </AuthProvider>
+          </JobDataProvider>
       </Router>
     
-      </>
+      </div>
   );
 }
+
 
 export default App;
