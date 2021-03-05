57297fea990b44b3972a298be875bde2cdac70bf
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-03-05 22:24:14 +0300

    some name fixing.

--- src/components/PostBox/index.js
+++ src/components/PostBox/index.js
@@ -1,28 +1,28 @@
 import React from 'react'
-import {Event, Person, Photo, YouTube, Create} from '@material-ui/icons';
-import './postbox.css'
+import {Event,  Photo, YouTube, Create} from '@material-ui/icons';
+import styles from  './postbox.module.css'
 
 const PostBox = () => {
 	return (
-		<div className="postBox_container">
-			<div className="postBox_input">
-				<Person style={{fontSize:48}} />
+		<div className={styles.postBox_container}>
+			<div className={styles.postBox_input}>
+				<img className={styles.avatar_img} src="https://randomuser.me/api/portraits/women/40.jpg" alt="avatar"/>
 				<input  type="text" placeholder="Start a post..." />
 			</div>
-			<div className="postBox_menu">
-				<div className="postBox_menu_item">
+			<div className={styles.postBox_menu}>
+				<div className={styles.postBox_menu_item}>
 					<Photo />
 					<span>Photo</span>
 				</div>
-				<div className="postBox_menu_item">
+				<div className={styles.postBox_menu_item}>
 					<YouTube />
 					<span>Video</span>
 				</div>
-				<div className="postBox_menu_item">
+				<div className={styles.postBox_menu_item}>
 					<Event />
 					<span>Event</span>
 				</div>
-				<div className="postBox_menu_item">
+				<div className={styles.postBox_menu_item}>
 					<Create />
 					<span>Write Article</span>
 				</div>
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import {PhotoCamera, PersonAdd,TurnedIn, Add} from '@material-ui/icons';
-import './sidebar.css'
+import  styles from './sidebar.module.css'
 
 const Sidebar = () => {
 	return (
-		<div className="sidebar">
-			<div className="sidebar_top">
-				<div className="welcome">
-					<div  className="bg_wheat"/>
-					<div className="avatar">
-						<PhotoCamera />
-					</div>
+		<div className={styles.sidebar}>
+			<div className={styles.sidebar_top}>
+				<div className={styles.welcome}>
+					<div  className={styles.bg_wheat}/>
+	
+						<img className={styles.avatar} src="https://randomuser.me/api/portraits/women/40.jpg" alt="avatar"/>
+				
 				
 					<h3>Welcome, User</h3>
 					<a href="/">Add a Photo</a>
 				</div>
-				<div className="connections">
+				<div className={styles.connections}>
 					<div>
 						<h4>Connections</h4>
 						<p>Grow your Network</p>
 					</div>
 					<PersonAdd />
 				</div>
-				<div className="items">
+				<div className={styles.items}>
 					<TurnedIn />
 					<h4 >My Items</h4>
 				</div>
 			</div>
-			<div className="sidebar_bottom">
-				<div className="sidebar_menu">
+			<div className={styles.sidebar_bottom}>
+				<div className={styles.sidebar_menu}>
 					<ul>
 						<li>Groups</li>
 						<li>Events</li>
 						<li>Followed Hashtags</li>
 					</ul>
-					<div className="add_btn">
+					<div className={styles.add_btn}>
 						<Add />
 					</div>
 				
