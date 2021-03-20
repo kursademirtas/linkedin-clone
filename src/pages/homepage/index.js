41f611159e26241590969e9c6e6d0b4808987f0c
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-03-20 14:33:40 +0300

    link added.

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-
 import PublicHeader from '../../components/PublicHeader';
-
-
+import { Link }from 'react-router-dom'
 import './homepage.css'
 
 function Homepage() {
@@ -14,9 +12,9 @@ function Homepage() {
 				<div classname="welcome_intent">
 					<h2 className="welcome_title">Welcome to your professional community</h2>
 					<ul className="intent_container">
-						<li className="intent">Search for a job<span>&gt;</span></li>
-						<li className="intent">Find a person you know<span>&gt;</span></li>
-						<li className="intent">Learn a new skill<span>&gt;</span></li>
+						<Link to="/jobs"> <li className="intent">Search for a job<span>&gt;</span></li> </Link>
+						<Link to="/people" > <li className="intent">Find a person you know<span>&gt;</span></li></Link>
+						<Link to="/learn">	<li className="intent">Learn a new skill<span>&gt;</span></li></Link>
 					</ul>
 				</div>
 				<img src="https://static-exp1.licdn.com/sc/h/d58zfe6h3ycgq5l1ccjpkrtdn" alt="illustiration " className="illustiration-homepage"/>
