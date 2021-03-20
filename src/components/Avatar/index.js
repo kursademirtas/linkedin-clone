0384d44bdb195ba8b5cf5659a1d0a45cd2c6e183
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-03-20 14:29:58 +0300

    avatar component added.

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,8 @@
+
+const Avatar = ({ size }) => {
+	return (
+	<img src="https://randomuser.me/api/portraits/women/40.jpg" alt="avatar" width={size} height={size} style={{borderRadius:'50%'}}/>
+	)
+}
+
+export default Avatar
