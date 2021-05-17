55c84b4a548d6d5fa0f064549310235d9965a847
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-05-17 23:13:41 +0300

    learning page added.

---
+++ src/pages/learning/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PublicHeader from '../../components/PublicHeader'
+
+const LearningPage = () => {
+	return (
+		<div>
+			<PublicHeader learningPage  />
+		</div>
+	)
+}
+
+export default LearningPage;
