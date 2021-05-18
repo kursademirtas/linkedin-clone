d5241997b6839be33293beabd95c4b882271f153
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-05-18 15:16:00 +0300

    people data added.

--- src/pages/people/index.js
+++ src/pages/people/index.js
@@ -1,20 +1,42 @@
-import React from 'react'
-import PublicHeader from '../../components/PublicHeader'
-import Footer from '../../components/Footer'
-import styles from './peoplePage.module.css'
+import React from "react";
+import { usePeopleData } from "../../contexts/PeopleDataContext";
+import PublicHeader from "../../components/PublicHeader";
+import Footer from "../../components/Footer";
+import styles from "./peoplePage.module.css";
+import PersonCard from "../../components/PersonCard";
 
 const PeoplePage = () => {
-	return (
-		<div className={styles.page_container}>
-			<PublicHeader  peoplePage={true}/>
-			<div className={styles.wrapper}>
-				<img src="https://static-exp1.licdn.com/sc/h/dfa1erc1uncnl1po7l2v7yawd" alt="an illustration about talking to two people on the blue couch" width='200px' />
-				<p>Try searching for your co-worker, classmate, professor, or friend.</p>
-			</div>
-			<Footer/>
-		</div>
-	
-	)
-}
+  const { peopleData,  searchResult, searchPerson } = usePeopleData();
 
-export default PeoplePage
+  const numberOfPerson = searchResult.length;
+
+  const HeroSection = () => {
+    return (
+      <div className={styles.wrapper}>
+        <img
+          src="https://static-exp1.licdn.com/sc/h/dfa1erc1uncnl1po7l2v7yawd"
+          alt="an illustration about talking to two people on the blue couch"
+          width="200px"
+        />
+        <p>
+          Try searching for your co-worker, classmate, professor, or friend.
+        </p>
+      </div>
+    );
+  };
+
+  return (
+    <div className={styles.page_container}>
+      <PublicHeader peoplePage={true} search={searchPerson} />
+      {numberOfPerson ? (
+        searchResult.map((person) => {
+          return <PersonCard key={person.id} person={person}/>;
+        })
+      ) : (
+        <HeroSection />
+      )}
+    </div>
+  );
+};
+
+export default PeoplePage;
