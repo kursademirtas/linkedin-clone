06d60cdaa4b5c9337835d904d8f26de39e5254cb
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-05-18 15:15:18 +0300

    added options and search props.

--- src/components/PublicHeader/index.js
+++ src/components/PublicHeader/index.js
@@ -1,28 +1,37 @@
-import React from 'react'
-import Logo from '../Logo';
-import { Link, useHistory } from 'react-router-dom';
-import styles from './publicHeader.module.css'
-import SearchBar from '../SearchBar';
+import React from "react";
+import Logo from "../Logo";
+import { Link, useHistory } from "react-router-dom";
+import styles from "./publicHeader.module.css";
+import SearchBar from "../SearchBar";
 
+const PublicHeader = ({ learningPage, peoplePage, search }) => {
+  const initialOption = peoplePage
+    ? "people"
+    : learningPage
+    ? "learning"
+    : "jobs";
 
-const PublicHeader = () => {
+  const history = useHistory();
+  const path = history.location.pathname;
 
-	const history = useHistory();
-	const path = history.location.pathname
+  return (
+    <header className={styles.header}>
+      <Logo className={styles.logo} />
+      <div className={styles.searchBar}>
+        {path !== "/" ? (
+          <SearchBar initialOption={initialOption} search={search} />
+        ) : null}
+      </div>
+      <div>
+        <Link to="/signup">
+          <button className={styles.btn_join}>Join Now</button>
+        </Link>
+        <Link to="/login">
+          <button className={styles.btn_login_small}>Sign in</button>
+        </Link>
+      </div>
+    </header>
+  );
+};
 
-	return (
-		<header className={styles.header}>
-			
-				<Logo className={styles.logo}/>
-				<div className={styles.searchBar}>
-					{path !== "/" ?  <SearchBar /> :null}
-				</div>
-				<div>
-					<Link to="/signup"><button className={styles.btn_join}>Join Now</button></Link>
-					<Link to="/login"><button className={styles.btn_login_small }>Sign in</button></Link>
-				</div>
-		</header>
-	)
-}
-
-export default PublicHeader
+export default PublicHeader;
