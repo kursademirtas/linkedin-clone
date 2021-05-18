c331904304eede088b09f15ca4f3ced20106dc1d
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-05-18 15:13:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Feed from './pages/feed'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Feed from "./pages/feed";
 import Login from "./pages/login";
-import Homepage from  './pages/homepage'
+import Homepage from "./pages/homepage";
 import { AuthProvider } from "./contexts/AuthContext";
-import { JobDataProvider } from './contexts/JobsDataContext';
+import { JobDataProvider } from "./contexts/JobsDataContext";
 import PrivateRoute from "./components/PrivateRoute";
 import Signup from "./pages/signup/Signup";
-import Jobs from './pages/jobs'
+import Jobs from "./pages/jobs";
 import { PostDataProvider } from "./contexts/PostDataContext";
-
+import LearningPage from "./pages/learning";
+import PeoplePage from "./pages/people";
+import { PeopleDataProvider } from "./contexts/PeopleDataContext";
 
 function App() {
-
   return (
-    
-      <div className="app">
+    <div className="app">
       <Router>
         <JobDataProvider>
-          <AuthProvider >
+          <AuthProvider>
             <PostDataProvider>
-            <Switch>
-                <Route path="/" exact component={Homepage} />
-                <Route path="/signup" component={Signup} />
-                <Route path="/login"  component={Login} />
-                <Route path="/jobs" component={Jobs} />
-                <PrivateRoute path="/feed" component={Feed} />
-            </Switch>
+              <PeopleDataProvider>
+                <Switch>
+                  <Route path="/" exact component={Homepage} />
+                  <Route path="/signup" component={Signup} />
+                  <Route path="/login" component={Login} />
+                  <Route path="/jobs" component={Jobs} />
+                  <Route path="/learning" component={LearningPage} />
+                  <Route path="/people" component={PeoplePage} />
+                  <PrivateRoute path="/feed" component={Feed} />
+                </Switch>
+              </PeopleDataProvider>
             </PostDataProvider>
-            </AuthProvider>
-          </JobDataProvider>
+          </AuthProvider>
+        </JobDataProvider>
       </Router>
-    
-      </div>
+    </div>
   );
 }
-
 
 export default App;
