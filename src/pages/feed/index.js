5eebb7a9737cc4ee06bf9969550a8a74e9d9a732
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-06-02 10:55:59 +0300

    scroll position passed child componenets and some style bug's fixed.

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -6,25 +6,33 @@ import Header from "../../components/Header";
 import styles from "./feed.module.css";
 import Post from "../../components/Post";
 import Widgets from "../../components/Widgets";
+import { useScrollPosition } from "../../hooks/useScrollPosition";
+import { useState } from "react/cjs/react.development";
 
 const Feed = () => {
   const { postData } = usePostData();
+  const [scrollY, setScrollY] = useState(0);
+
+  useScrollPosition(({ prevPos, currPos }) => {
+    setScrollY(currPos.y)
+  });
 
   const sortedData = postData.sort(function (x, y) {
     return y.date - x.date;
   });
+
   return (
     <div className={styles.feed_container}>
       <Header />
       <div className={styles.feed_body}>
-        <Sidebar />
-        <div>
+        <Sidebar scrollY={scrollY} />
+        <div className={styles.flow}>
           <PostBox />
           {sortedData.map((post) => (
             <Post post={post} />
           ))}
         </div>
-        <Widgets />
+        <Widgets scrollY={scrollY} />
       </div>
     </div>
   );
