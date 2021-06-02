1b2013dd0f5e9740a16e9088a47be7fe684ee054
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-06-02 10:35:20 +0300

    Login page style bug's fixed.

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,66 +1,79 @@
-import React, {useState, useRef} from 'react';
-import { useAuth } from '../../contexts/AuthContext';
-import {Link, useHistory} from 'react-router-dom';
-import { Alert } from '@material-ui/lab'
-import Logo from '../../components/Logo'
-import './login.css'
-
+import React, { useState, useRef } from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
+import { Alert } from "@material-ui/lab";
+import Logo from "../../components/Logo";
+import styles from "./LoginPage.module.css";
+import Footer from "../../components/Footer";
 
 const Login = () => {
+  const history = useHistory();
+  const { currentUser } = useAuth();
 
-	const history = useHistory();
-	const { currentUser } = useAuth();
+  if (currentUser) history.push("/feed");
 
-	if(currentUser) history.push("/feed");
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { login } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
 
-	const emailRef = useRef();
-	const passwordRef = useRef();
-	const {login} = useAuth();
-	const [error, setError] = useState("");
-	const [loading, setLoading] = useState(true);
-	
-	
-	async function handleSubmit(e) {
-		e.preventDefault();
-		console.log(emailRef.current.value)
-		try {
-			setError("");
-			setLoading(true);
-			await login(emailRef.current.value, passwordRef.current.value);
-			history.push("/feed")
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      setError("");
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      history.push("/feed");
+    } catch {
+      setError("Failed to login");
+    }
+    setLoading(false);
+  }
 
-		}catch {
-			setError("Failed to login")
-			
-		}
-		setLoading(false)
-	}
+  return (
+    <div className={styles.login_container}>
+      <div>
+        <Logo />
+        <div className={styles.form_container}>
+          {error && <Alert severity="error">{error}</Alert>}
+          <div>
+            <h2 className={styles.login_title}>Sign in</h2>
+            <p className={styles.login_text}>
+              Stay updated on your professsional world
+            </p>
+          </div>
 
-	return (
-		<div className="login_container">
-			<Logo />
-			<div className="form_container">
-				{error &&  <Alert severity="error">{error}</Alert>}
-				<div>
-					<h2 className="login_title">Sign in</h2>
-					<p className="login_text">Stay updated on your professsional world</p>
-				</div>
+          <form className={styles.form_elements} onSubmit={handleSubmit}>
+            <input
+              className={styles.login_input}
+              placeholder="Email or Phone"
+              type="email"
+              ref={emailRef}
+              required
+            />
+            <input
+              className={styles.login_input}
+              placeholder="Password"
+              type="password"
+              ref={passwordRef}
+              required
+            />
+            <Link to='/resetpassword' className={styles.btn_primary}>Forgot password?</Link>
+            <input className={styles.btn_login} type="submit" value="Sign in" />
+          </form>
+        </div>
 
-				<form className="form_elements" onSubmit={handleSubmit}>
-					<input className="login_input" placeholder="Email or Phone"  type="email" ref={emailRef} required />
-					<input className="login_input"  placeholder="Password"  type="password" ref={passwordRef} required />
-					<button className="btn_primary">Forgot password?</button>
-					<input className="btn_login" type="submit"  value="Sign in"/>
-				</form>
-			</div>
-
-			<div>
-				<p className="login_text">New to Linkedin?</p>
-				<button disabled={loading} className="btn_primary">Join Now</button>
-			</div>
-
-		</div>
-	)
-}
+        <div className={styles.wrapper}>
+          <p className={styles.login_text}>New to Linkedin?</p>
+          <Link to='/signup' disabled={loading} className={styles.btn_primary}>
+            Join Now
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Login;
